60e35d0156e7fca347a55a02e91ed1be91b4d5b1
Davit Hakobyan <davit@innorise.com>
2021-07-16 19:09:52 +0400

    Made changes to messages controller

--- src/main/java/com/daves/chat/controller/MessagesController.java
+++ src/main/java/com/daves/chat/controller/MessagesController.java
@@ -3,11 +3,8 @@ package com.daves.chat.controller;
 import com.daves.chat.model.Message;
 import com.daves.chat.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,16 +18,8 @@ public class MessagesController {
     }
 
     @PostMapping("/messages")
-    public ResponseEntity<Object> addMessage(@RequestBody Message message) {
-        Message createdMessage = messageRepository.save(message);
-
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(createdMessage.getId())
-                .toUri();
-
-        return ResponseEntity.created(location).build();
+    public Message addMessage(@RequestBody Message message) {
+        return messageRepository.save(message);
     }
 
 }
